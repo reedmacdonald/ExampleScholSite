e8067e443146180928163d18ab8b418105ee5aab
Reed MacDonald <reedpmacdonald@gmail.com>
2020-08-18 17:32:51 -0700

    Added exclusives page

--- gatsby-node.js
+++ gatsby-node.js
@@ -100,3 +100,50 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     });
   });
 };
+
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const result = await graphql(`
+    {
+      allContentfulExclusive {
+        nodes {
+          body {
+            json
+          }
+          dateClosed
+          createdAt
+          title
+          description
+          dripId
+          slug
+          headerImage {
+            description
+            file {
+              url
+            }
+            title
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    reporter.panic('failed to create posts', result.errors);
+  }
+
+  const posts = result.data.allContentfulExclusive.nodes;
+
+  posts.forEach((post, index) => {
+    console.log(post, '<---post');
+    actions.createPage({
+      path: `exclusive/${post.slug}`,
+      component: require.resolve('./src/templates/exclusives.js'),
+      context: {
+        slug: post.slug,
+        title: post.title,
+        body: post.body,
+        image: post.headerImage.file.url,
+      },
+    });
+  });
+};
---
+++ src/templates/exclusives.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { BLOCKS, MARKS } from '@contentful/rich-text-types';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { css } from '@emotion/core';
+import Layout from '../components/layout';
+import ReadLink from '../components/read-link';
+
+const Exclusives = props => {
+  return (
+    <Layout>
+      <div>SWelcome to the Exclusives Page</div>
+      <h1>{props.pageContext.title}</h1>
+      <h4>{props.pageContext.description}</h4>
+      <div style={{ maxWidth: '200px' }}>
+        <img
+          style={{ maxWidth: '300px' }}
+          src={`https:${props.pageContext.image}`}
+        />
+      </div>
+      <div>
+        {documentToReactComponents(props.pageContext.body.json, {
+          renderNode: {
+            [BLOCKS.EMBEDDED_ASSET]: (node, children) => (
+              <img src={`${node.data.target.fields.file['en-US'].url}?w=300`} />
+            ),
+          },
+        })}
+      </div>
+
+      <br />
+      <ReadLink to="/">&larr; back to all posts</ReadLink>
+    </Layout>
+  );
+};
+
+export default Exclusives;
