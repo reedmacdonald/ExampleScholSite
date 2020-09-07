dd14d2a14990ad18a92b28da9a294f45b1601692
Reed MacDonald <reedpmacdonald@gmail.com>
2020-09-07 13:23:07 -0700

    Flailing here

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,6 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
-    `gatsby-transformer-remark`,
     {
       resolve: 'gatsby-plugin-mdx',
       options: {
