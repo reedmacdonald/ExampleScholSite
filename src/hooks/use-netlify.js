51dde913d5c81dadaaddbef09124b9779c8a3e0c
Reed MacDonald <reedpmacdonald@gmail.com>
2020-09-07 07:29:17 -0700

    Added terms

--- src/hooks/use-netlify.js
+++ src/hooks/use-netlify.js
@@ -2,18 +2,7 @@ import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 
 const ComponentName = () => {
-  const data = useStaticQuery(graphql`
-    {
-      allMarkdownRemark(filter: { frontmatter: { title: { eq: "hello" } } }) {
-        edges {
-          node {
-            rawMarkdownBody
-          }
-        }
-      }
-    }
-  `);
-  return data.allMarkdownRemark.edges[0].node.rawMarkdownBody;
+  return 'hello world';
 };
 
 export default ComponentName;
