51646d970e7b01a0dc67e85b6dbe4bf7ede2bdb4
Reed MacDonald <reedpmacdonald@gmail.com>
2020-09-07 17:05:38 -0700

    Kind of drunk now but hoping this works

---
+++ public/component---src-pages-about-js-d401debda18bf91239c4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{"3XHS":function(t,c,e){"use strict";e.r(c);e("q1tI");var n=e("Wbzz"),o=e("Bl7J"),b=e("qKvR");c.default=function(){return Object(b.c)(o.a,null,Object(b.c)("h1",null,"About Me"),Object(b.c)("p",null,"This is my personal website."),Object(b.c)(n.a,{to:"/"},"← back to home"))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-d401debda18bf91239c4.js.map
